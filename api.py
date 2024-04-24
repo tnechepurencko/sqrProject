c5a94cb03ffc66e4fe73615ab998f97233239d83
Alyona-art <alena.artemeva.2002.15@gmail.com>
2024-04-24 23:31:01 +0300

    tasks with Nix stores

--- api.py
+++ api.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from starlette import status
 from starlette.responses import Response
+from pathlib import Path
+from shutil import rmtree
+import subprocess
+import json
 
 from db import Users
 
 app = FastAPI()
 users_db = Users()
+
+nix_stores_path = './nix_stores'
 
 
 def one_param_check(p1):
@@ -44,44 +50,163 @@ def registration(uname, pwd):
 
 @app.post('/create_store')
 def create_store(uname, sname):
+    try:
+        store_path = Path(f'{nix_stores_path}/{uname}/{sname}')
+        store_path.mkdir(parents=True)
+        # TODO : Add store to database
+    except FileExistsError:
+        return get_response(status.HTTP_400_BAD_REQUEST, 'Store already exists')
+    return get_response(status.HTTP_200_OK, one_param_check(store_path))
+
+
+@app.post('/get_stores')
+def get_stores(uname):
+    # TODO: get all stores for thus uname from database
     pass
 
 
 @app.post('/remove_store')
 def remove_store(uname, sname):
-    pass
-
-
-@app.post('/track_store')
-def track_store(sname):
-    pass
+    try:
+        store_path = Path(f'{nix_stores_path}/{uname}/{sname}')
+        rmtree(store_path)
+        # TODO : Remove store from database
+    except Exception as e:
+        return get_response(status.HTTP_400_BAD_REQUEST, e)
+    return get_response(status.HTTP_200_OK, one_param_check(store_path))
 
 
 @app.post('/add_package')
-def add_package(pname, sname):
-    pass
+def add_package(uname, sname, pname):
+    proc = subprocess.run(
+        [
+            'nix',
+            'build',
+            '--json',
+            '--no-link',
+            '--store',
+            f'{nix_stores_path}/{uname}/{sname}',
+            f'nixpkgs#{pname}',
+        ],
+        capture_output=True,
+        text=True)
+    if proc.returncode != 0:
+        return get_response(status.HTTP_400_BAD_REQUEST, proc.returncode)
+    output = json.loads(proc.stdout)
+    package_path = output[0]["outputs"]["out"]
+    return get_response(status.HTTP_200_OK, one_param_check(package_path))
 
 
 @app.post('/rem_package')
-def rem_package(pname, sname):
-    pass
+def rem_package(uname, sname, pname):
+    # Delete package
+    proc = subprocess.run(
+        [
+            'nix',
+            'store',
+            'delete',
+            '--store',
+            f'{nix_stores_path}/{uname}/{sname}',
+            f'nixpkgs#{pname}',
+        ],
+        capture_output=True,
+        text=True)
+    if proc.returncode != 0:
+        return get_response(status.HTTP_400_BAD_REQUEST, proc.returncode)
+
+    # Collect garbage
+    proc = subprocess.run(
+        [
+            'nix',
+            'store',
+            'gc',
+            '--store',
+            f'{nix_stores_path}/{uname}/{sname}',
+        ],
+        capture_output=True,
+        text=True)
+    if proc.returncode != 0:
+        return get_response(status.HTTP_400_BAD_REQUEST, proc.returncode)
+
+    return get_response(status.HTTP_200_OK, pname)
 
 
 @app.get('/dif_paths')
-def dif_paths(sname1, sname2):
-    pass
+def dif_paths(uname, sname1, sname2):
+    store_path1 = Path(f'{nix_stores_path}/{uname}/{sname1}/nix/store')
+    store_path2 = Path(f'{nix_stores_path}/{uname}/{sname2}/nix/store')
+
+    set1 = set([f'/nix/store/{path}' for path in store_path1.iterdir()])
+    set2 = set([f'/nix/store/{path}' for path in store_path2.iterdir()])
+
+    return get_response(status.HTTP_200_OK, list(set1 - set2))
 
 
 @app.get('/dif_package')
-def dif_package(package1, package2):
-    pass
+def dif_package(uname, sname1, pname1, sname2, pname2):
+    # Get first closure paths
+    proc = subprocess.run(
+        [
+            'nix',
+            'path-info',
+            '--recursive',
+            '--store',
+            f'{nix_stores_path}/{uname}/{sname1}',
+            f'nixpkgs#{pname1}',
+        ],
+        capture_output=True,
+        text=True)
+    if proc.returncode != 0:
+        return get_response(status.HTTP_400_BAD_REQUEST, proc.returncode)
+    set1 = set(proc.stdout.split())
+
+    # Get second closure paths
+    proc = subprocess.run(
+        [
+            'nix',
+            'path-info',
+            '--recursive',
+            '--store',
+            f'{nix_stores_path}/{uname}/{sname2}',
+            f'nixpkgs#{pname2}',
+        ],
+        capture_output=True,
+        text=True)
+    if proc.returncode != 0:
+        return get_response(status.HTTP_400_BAD_REQUEST, proc.returncode)
+    set2 = set(proc.stdout.split())
+
+    # Get difference
+    return get_response(status.HTTP_200_OK, list(set1 - set2))
 
 
 @app.get('/size_package')
-def size_package(package):
-    pass
+def size_package(uname, sname, pname):
+    proc = subprocess.run(
+        [
+            'nix',
+            'path-info',
+            '--json',
+            '--recursive',
+            '--closure-size',
+            '--store',
+            f'{nix_stores_path}/{uname}/{sname}',
+            f'nixpkgs#{pname}',
+        ],
+        capture_output=True,
+        text=True)
+    if proc.returncode != 0:
+        return get_response(status.HTTP_400_BAD_REQUEST, proc.returncode)
+
+    output = json.loads(proc.stdout)
+    closure_size = sum(path["closureSize"] for path in output)
+    return get_response(status.HTTP_200_OK, closure_size)
 
 
 @app.get('/package_exists')
-def package_exists(package):
-    pass
+def package_exists(uname, sname, pname):
+    store_path1 = Path(f'{nix_stores_path}/{uname}/{sname}/nix/store')
+    for path in store_path1.iterdir():
+        if pname in path:
+            return get_response(status.HTTP_400_BAD_REQUEST, True)
+    return get_response(status.HTTP_400_BAD_REQUEST, False)
---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
